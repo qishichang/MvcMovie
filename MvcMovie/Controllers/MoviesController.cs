91d3b4be94f4d75f4cdd8b3df3471e06f0b0026a
qishichang <qishichang@live.com>
2018-10-19 10:38:15 +1300

    rename searchString to id

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -19,13 +19,13 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string id)
         {
             var movies = from m in _context.Movie
                          select m;
-            if (!string.IsNullOrEmpty(searchString))
+            if (!string.IsNullOrEmpty(id))
             {
-                movies = movies.Where(s => s.Title.Contains(searchString));
+                movies = movies.Where(s => s.Title.Contains(id));
             }
             return View(await movies.ToListAsync());
         }
